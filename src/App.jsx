24fcb104014f20a1b7727f89103e0d7c2ecf35f3
estebandonis <estebandonis10@gmail.com>
2024-08-05 17:33:53 -0600

    Manage to retrieve all contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { client } from '@xmpp/client'
+import { client, xml } from '@xmpp/client'
 
 function App() {
+  const [contacts, setContacts] = useState([])
 
   useEffect(() => {
     const xmpp = client({
@@ -24,6 +25,44 @@ function App() {
       console.log('XMPP Status:', status);
     });
 
+    xmpp.on('online', async (address) => {
+      console.log('Connected as', address.toString());
+
+      // // Request the roster
+      // const rosterIQ = xml('iq', { type: 'get', id: 'roster1' },
+      //   xml('query', { xmlns: 'jabber:iq:roster' })
+      // );
+
+      const rosterIQ = xml(
+        'iq',
+        { type: 'get', id: 'roster_1' },
+        xml('query', { xmlns: 'jabber:iq:roster' })
+      );
+
+      try {
+        const response = await xmpp.send(rosterIQ);
+        console.log(response);
+        // const items = response.getChild('query').getChildren('item');
+        // const contactsList = items.map(item => ({
+        //   jid: item.attrs.jid,
+        //   name: item.attrs.name,
+        //   subscription: item.attrs.subscription,
+        // }));
+        // setContacts(contactsList);
+
+        const result = await xmpp.iqCaller.request(rosterIQ);
+        const contacts = result.getChild('query').getChildren('item');
+
+        console.log('Contacts:');
+        contacts.forEach(contact => {
+          console.log(`- JID: ${contact.attrs.jid}, Name: ${contact.attrs.name || 'N/A'}`);
+        });
+          // setContacts(contacts);
+      } catch (err) {
+        console.error('Failed to fetch roster:', err);
+      }
+    });
+
     xmpp.start().catch(console.error)
 
     xmpp.stop().catch(console.error)
@@ -33,7 +72,13 @@ function App() {
   return (
     <>
       <h1>XMPP</h1>
-
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.jid}>
+            {contact.name || contact.jid}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
