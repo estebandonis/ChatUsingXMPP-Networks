9f719ec59591a73435d535a4232dded6049e45d2
estebandonis <estebandonis10@gmail.com>
2024-08-11 12:17:17 -0600

    Receiving messages achieved

--- src/components/XMPPChat.jsx
+++ src/components/XMPPChat.jsx
@@ -11,33 +11,20 @@ const XMPPChat = () => {
     const [password, setPassword] = useState('admin123');
 
     useEffect(() => {
+        console.log('Contacts:', contacts);
+        contacts.forEach(contact => {
+            const presenceStanza = xml('presence', { to: contact.jid, type: 'subscribe' });
+            xmpp.send(presenceStanza);
+        });
+    }, [contacts]);
+
+    useEffect(() => {
         const xmppClient = client({
             service: 'ws://alumchat.lol:7070/ws/',
             domain: 'alumchat.lol',
             username: user,
             password: 'admin123',
-        });
-
-        // Set up message handler
-        xmppClient.on('stanza', async (stanza) => {
-            console.log('Stanza:', stanza.toString());
-            if (stanza.is('message') && stanza.getChild('body')) {
-                // const { from, body } = stanza.attrs;
-
-                const from = stanza.attr('from');
-                const body = stanza.attr('body');
-                console.log('Message:', from, body);
-                setMessages((prevMessages) => [...prevMessages, { from, body }]);
-            }
-
-            // Handle presence stanzas
-            if (stanza.is('presence')) {
-                const from = stanza.attr('from');
-                const type = stanza.attr('type');
-                const show = stanza.getChild('show')?.text();
-                console.log('Presence:', from, type, show);
-                updateContactStatus(from, type);
-            }
+            resource: 'web',
         });
 
         xmppClient.on('online', async (address) => {
@@ -45,7 +32,7 @@ const XMPPChat = () => {
 
             console.log('Connected as', address.toString());
             setStatus('online');
-            
+
             const rosterStanza = xml('iq', { type: 'get' }, xml('query', { xmlns: 'jabber:iq:roster' }));
             const rosterResult = await xmppClient.iqCaller.request(rosterStanza);
 
@@ -58,15 +45,57 @@ const XMPPChat = () => {
             }));
             setContacts(contactList);
 
+            const messageLogStanza = xml('iq', { type: 'get', id: 'message-log' }, xml('query', { xmlns: 'urn:xmpp:mam:2' }));
+            const result = await xmpp.iqCaller.request(messageLogStanza);
+            const messages = result.getChild('fin').getChildren('result').map(result => {
+                const message = result.getChild('forwarded').getChild('message');
+                return {
+                    from: message.attr('from'),
+                    body: message.getChildText('body'),
+                };
+            });
+            console.log('Messages:', messages);
+            setMessages(messages);
+
             // Request presence information for all contacts
             contactList.forEach(contact => {
-                const presenceStanza = xml('presence', { to: contact.jid });
+                const presenceStanza = xml('presence', { to: contact.jid, type: 'subscribe' });
                 xmppClient.send(presenceStanza);
             });
         });
 
         xmppClient.on('offline', () => {
             console.log('Connection has been closed');
+        });
+
+        // Set up message handler
+        xmppClient.on('stanza', async (stanza) => {
+            // console.log('Stanza:', stanza.toString());
+            if (stanza.is('message') && stanza.getChild('body')) {
+                console.log('Message Stanza:', stanza.toString());
+
+                const fromLong = stanza.attr('from');
+                const from = fromLong.split('/')[0];
+                const body = stanza.getChild('body')?.text();
+                console.log('Message:', from, body);
+                setMessages((prevMessages) => [...prevMessages, { from, body }]);
+            }
+
+            // Handle presence stanzas
+            if (stanza.is('presence')) {
+                console.log('Presence Stanza:', stanza.toString());
+                const from = stanza.attr('from');
+                const bareJid = from.split('/')[0];
+                const type = stanza.attr('type');
+                const show = stanza.getChild('show')?.text();
+                console.log('Presence:', bareJid, type, show);
+                console.log('Contacts in presence:', contacts);
+                updateContactStatus(bareJid, type ? type : show ? show : 'online');
+            }
+
+            if (stanza.is('iq')){
+                console.log('IQ:', stanza.toString());
+            }
         });
 
         xmppClient.start().catch(console.error);
@@ -82,6 +111,8 @@ const XMPPChat = () => {
     }, [messages]);
 
     const updateContactStatus = (jid, status) => {
+        console.log('Updating contact status:', jid, status);
+        console.log('Contacts:', contacts);
         setContacts(prevContacts =>
             prevContacts.map(contact =>
                 contact.jid === jid ? { ...contact, status } : contact
@@ -105,9 +136,9 @@ const XMPPChat = () => {
     const setPresence = (status) => {
         if (xmpp) {
             let presenceStanza;
-            if (status === 'offline') {
+            if (status === 'unavailable') {
                 presenceStanza = xml('presence', { type: 'unavailable' });
-            } else if (status === 'online') {
+            } else if (status === 'available') {
                 presenceStanza = xml('presence');
             }else {
                 presenceStanza = xml('presence', {}, xml('show', {}, status));
@@ -119,6 +150,9 @@ const XMPPChat = () => {
 
     return (
         <div>
+            <div>
+                <h2>Status: {status}</h2>
+            </div>
             <div>
                 <h2>Contacts</h2>
                 {contacts.map((contact, index) => (
@@ -143,11 +177,11 @@ const XMPPChat = () => {
                 />
                 <button type="submit">Send</button>
 
-                <button onClick={() => setPresence('online')}>Available</button>
+                <button onClick={() => setPresence('available')}>Available</button>
                 <button onClick={() => setPresence('away')}>Away</button>
                 <button onClick={() => setPresence('dnd')}>Do Not Disturb</button>
                 <button onClick={() => setPresence('xa')}>Extended Away</button>
-                <button onClick={() => setPresence('offline')}>Offline</button>
+                <button onClick={() => setPresence('unavailable')}>Offline</button>
             </form>
         </div>
     );
